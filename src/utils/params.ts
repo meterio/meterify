57397965a42d565ad7a9cbcb438509b6ff5a4c0b
Simon.Z <zhanghan.simon@gmail.com>
2024-01-18 14:02:36 -0800

    set defaultGasPriceCoef to be 0 instead of 128

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -1,16 +1,14 @@
-'use strict'
+"use strict";
 
 // params from meter source code and meter foundation's suggestion
 const params = {
-    defaultGasPriceCoef: 128,
-    defaultExpiration: 720,
-    TxGas: 5000,
-    ClauseGas: 21000 - 5000,
-    ClauseGasContractCreation: 53000 - 5000,
-    TxDataZeroGas: 4,
-    TxDataNonZeroGas: 68,
-}
+  defaultGasPriceCoef: 0,
+  defaultExpiration: 720,
+  TxGas: 5000,
+  ClauseGas: 21000 - 5000,
+  ClauseGasContractCreation: 53000 - 5000,
+  TxDataZeroGas: 4,
+  TxDataNonZeroGas: 68,
+};
 
-export {
-    params,
-}
+export { params };
